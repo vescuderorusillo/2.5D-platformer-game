368ada5aba69e9ed5a06eb16cd4efc6079ea1f57
Victor Escudero <victor.escudero@bestsecret.com>
2021-02-06 21:21:57 +0100

    Implementing Gravity and Jumping for the Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,16 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _speed = 5f;
 
+    [SerializeField]
+    private float _gravity = 1.0f;
+
+    [SerializeField]
+    private float _jumpHeight = 25.0f;
+
+    private float _yVelocity;
+
+    private bool _canDoubleJump;
+
     private void Start()
     {
         _controller = GetComponent<CharacterController>();
@@ -18,6 +28,28 @@ public class Player : MonoBehaviour
         var direction = new Vector3(horizontalInput, 0, 0);
 
         var velocity = direction * _speed;
+
+        if (_controller.isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight;
+                _canDoubleJump = true;
+            }
+        }
+        else
+        {
+            if (_canDoubleJump && Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight;
+                _canDoubleJump = false;
+            }
+            _yVelocity -= _gravity;
+        }
+
+        velocity.y = _yVelocity;
         _controller.Move(velocity * Time.deltaTime);
+
+
     }
 }
