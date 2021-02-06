8b0007dc4b83d9e397d5268983105be67cca97b6
Victor Escudero <victor.escudero@bestsecret.com>
2021-02-06 20:37:20 +0100

    Added Horizontal movement and velocity to the Player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+
+    [SerializeField]
+    private float _speed = 5f;
+
+    private void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        var horizontalInput = Input.GetAxis("Horizontal");
+        var direction = new Vector3(horizontalInput, 0, 0);
+
+        var velocity = direction * _speed;
+        _controller.Move(velocity * Time.deltaTime);
+    }
+}
