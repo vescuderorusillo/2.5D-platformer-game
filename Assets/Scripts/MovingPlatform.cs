eddde6be927bf19120bbf448395715ae5d0297eb
Victor Escudero <victor.escudero@bestsecret.com>
2021-02-07 20:26:46 +0100

    Implemented logic to move a platform

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _targetA, _targetB;
+
+    [SerializeField]
+    private float _speed = 1.0f;
+
+    private bool movingToB;
+
+    void Update()
+    {
+        if (transform.position == _targetA.position)
+        {
+            movingToB = true;
+        }
+        else if (transform.position == _targetB.position)
+        {
+            movingToB = false;
+        }
+
+        if (movingToB)
+        {
+            MoveTo(_targetB.position);
+        }
+        else
+        {
+            MoveTo(_targetA.position);
+        }
+
+    }
+
+    private void MoveTo(Vector3 targetPosition)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * _speed);
+    }
+}
